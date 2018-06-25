05a3bf043d598ea0203a69e42f6445775bd59b4f
Adam Borowski <adam.borowski@netguru.co>
2018-06-25 14:36:53 +0200

    test(components/Badge): configure enzyme for testing

--- src/components/Badge/test.js
+++ src/components/Badge/test.js
@@ -1,7 +1,12 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+
 import { Badge } from '.'
 
 describe('Badge', () => {
-  it('is truthy', () => {
-    expect(Badge).toBeTruthy()
+  it('displays text', () => {
+    const text = 'Some text'
+    const wrapper = shallow(<Badge text={text} />)
+    expect(wrapper.text()).toEqual(text)
   })
 })
