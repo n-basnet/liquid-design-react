558dc2eead10aede7a9a15a9dc4d6c6bbedec989
Adam Borowski <adam.borowski@netguru.co>
2018-06-26 14:00:11 +0200

    build(rollup): move some deps do dependencies; mark all as external in rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 import babel from 'rollup-plugin-babel'
 import commonjs from 'rollup-plugin-commonjs'
-import external from 'rollup-plugin-peer-deps-external'
+import autoExternal from 'rollup-plugin-auto-external'
 import postcss from 'rollup-plugin-postcss'
 import resolve from 'rollup-plugin-node-resolve'
 import url from 'rollup-plugin-url'
@@ -20,7 +20,7 @@ export default {
     },
   ],
   plugins: [
-    external(),
+    autoExternal(),
     postcss({
       modules: true,
     }),
