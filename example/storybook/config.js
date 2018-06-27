55ea639f55b784f357cc60700231e86361ed4c70
Adam Borowski <adam.borowski@netguru.co>
2018-06-27 13:24:41 +0200

    refactor(storybook): load components list from an array [MER-13]

--- example/storybook/config.js
+++ example/storybook/config.js
@@ -1,7 +1,11 @@
 import { configure } from '@storybook/react'
 
+import COMPONENTS from './components.json'
+
 function loadStories() {
-  require('./components/Badge.js')
+  COMPONENTS.map(v => {
+    require(`./components/${v}.js`)
+  })
 }
 
 configure(loadStories, module)
