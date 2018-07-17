d11b4a38ce99cf72a7714e9ccdc6d40c1a7b9aed
Adam Borowski <adam.borowski@netguru.co>
2018-07-17 17:23:02 +0200

    docs(storybook): add Breadcrumbs interactive story

--- storybook/components/Breadcrumbs.js
+++ storybook/components/Breadcrumbs.js
@@ -4,6 +4,7 @@ import { action } from '@storybook/addon-actions'
 
 import { Breadcrumbs } from '~'
 import { getBackgroundWrapper } from '../helpers'
+import { times } from '~/utils/aux'
 
 const getItems = (modifier = {}) => [
   {
@@ -12,7 +13,7 @@ const getItems = (modifier = {}) => [
   },
   {
     name: 'Breadcrumb 2',
-    ...!modifier.disabled && { onClick: action('click breadcrumb 2') },
+    ...(!modifier.disabled && { onClick: action('click breadcrumb 2') }),
     ...modifier,
   },
   {
@@ -21,21 +22,36 @@ const getItems = (modifier = {}) => [
   },
 ]
 
+const getItemsForInteractive = onClick =>
+  times(3).map(v => ({
+    name: `Breadcrumb ${v + 1}`,
+    onClick: () => onClick(v),
+  }))
+
+class BreadcrumbsApp extends React.Component {
+  state = { active: null }
+  setActive = active => {
+    this.setState({ active })
+  }
+  render() {
+    return (
+      <Breadcrumbs
+        items={getItemsForInteractive(this.setActive)}
+        active={this.state.active}
+      />
+    )
+  }
+}
+
 storiesOf('Components/Breadcrumbs', module)
   .addDecorator(getBackgroundWrapper())
-  .add('default', () => (
-    <Breadcrumbs
-      items={getItems()}
-    />
-  ))
-  .add('active', () => (
-    <Breadcrumbs
-      items={getItems()}
-      active={1}
-    />
-  ))
-  .add('disabled', () => (
-    <Breadcrumbs
-      items={getItems({ disabled: true })}
-    />
-  ))
+  .add('default', () => <Breadcrumbs items={getItems()} />)
+  .add('active', () => <Breadcrumbs items={getItems()} active={1} />)
+  .add('disabled', () => <Breadcrumbs items={getItems({ disabled: true })} />)
+  .addParameters({
+    info: {
+      source: false,
+      propTablesExclude: [BreadcrumbsApp],
+    },
+  })
+  .add('interactive', () => <BreadcrumbsApp />)
