38c3595435841f0f7a799cd00ad2b92557eebb68
Adam Borowski <adam.borowski@netguru.co>
2018-11-28 15:12:57 +0100

    feat: remove Tabs component from codebase
    MER-625

--- src/components/Tabs/propTypes.js
+++
@@ -1,7 +0,0 @@
-import PropTypes from 'prop-types'
-
-export const tabsPropTypes = {
-  children: PropTypes.node.isRequired,
-  id: PropTypes.number.isRequired,
-  selectedTabId: PropTypes.number.isRequired,
-}
