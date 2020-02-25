30e8feddc4e72967baae55a86762ae55bb19172a
Boris <boris.diakur@merckgroup.com>
2020-02-25 20:55:44 +0100

    ci(cleanup): remove circleci stuff

--- deployment/config/deploy.rb
+++
@@ -1,5 +0,0 @@
-lock '3.9.0'
-
-set :application, "merck-react"
-set :repo_url, "git@gitlab.com:liquid-design/liquid-design-react.git"
-set :deploy_to, -> { "/home/deploy/apps/#{fetch(:application)}" }
--- deployment/config/deploy/staging.rb
+++
@@ -1,8 +0,0 @@
-server 'merck-react.staging.devguru.co', user: 'deploy', roles: %w[app db web]
-set :branch, 'release'
-set :capose_commands, -> {
-  [
-    'build',
-    'up -d web'
-  ]
-}
